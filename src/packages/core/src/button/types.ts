aaeccc6bf9e755798fc53a0e970cd43f6ea5ea84
lobyntsev <yakutoc@gmail.com>
2020-08-27 14:35:31 +0700

    fix(button): add ButtonHTMLAttributes to ButtonProps interface

--- src/packages/core/src/button/types.ts
+++ src/packages/core/src/button/types.ts
@@ -1,7 +1,7 @@
-import { ReactNode, MouseEventHandler } from 'react';
+import { ButtonHTMLAttributes, PropsWithChildren } from 'react';
 import { CommonAttributes } from '../../../../common';
 
-export interface ButtonProps extends CommonAttributes {
+interface CustomProps extends CommonAttributes, ButtonHTMLAttributes<HTMLButtonElement> {
   /**
    * Visual appearance.
    * @default default
@@ -9,34 +9,15 @@ export interface ButtonProps extends CommonAttributes {
   appearance?: 'default' | 'danger' | 'primary';
 
   /**
-   * Button contents.
-   */
-  children: ReactNode;
-
-  /**
    * Additional CSS class.
    */
   className?: string;
-
-  /**
-   * Button disabled state.
-   */
-  disabled?: boolean;
-
-  /**
-   * Click handler.
-   */
-  onClick?: MouseEventHandler<HTMLButtonElement>
 
   /**
    * Button size.
    * @default default
    */
   size?: 'default' | 'large' | 'small';
-
-  /**
-   * Button type.
-   * @default button
-   */
-  type?: 'button' | 'reset' | 'submit';
 }
+
+export type ButtonProps = PropsWithChildren<CustomProps>;
